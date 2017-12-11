05fd66fb5cd8bb8013d4cd6049a5982537b1ce82
DESKTOP-CVL4VDD\Marcel <s0552384@htw-berlin.de>
2017-12-11 19:17:17 +0300

    add canvas panel and coin that disappears on collision

---
+++ Assets/Scripts/ObjectsToCollect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsToCollect : MonoBehaviour {
+
+    void OnTriggerEnter(Collider player) {
+        if (player.gameObject.tag == "Player") gameObject.SetActive(false);
+    }
+}
